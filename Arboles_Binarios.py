cd4a84a0087f1e6e8bdc57aca2d8e503ce82f5bc
FerUgalde <118574317+FerUgalde@users.noreply.github.com>
2023-03-28 13:55:58 -0700

    Update Arboles_Binarios.py

--- Arboles_Binarios.py
+++ Arboles_Binarios.py
@@ -49,21 +49,51 @@ class Tree:
    
     def contains(self, value):
         next = self.root
+        tof = 'x'
 
-        while True:
+        while tof == 'x':
             if value == next.value:
                 print(f"\n{value} si se encuentra en el arbol\n")
-                return False
+                tof = 't'
+                return tof
             elif next.value > value:  
                 if next.left == None:
                     print(f"\n{value} no se encuentra en el arbol")
-                    return False 
+                    tof = 'f'
+                    return tof 
                 next = next.left
             elif next.value < value:
                 if next.right == None:
                     print(f"\n{value} no se encuentra en el arbol")
-                    return False 
+                    tof = 'f'
+                    return tof
                 next = next.right
+
+
+    def closest_value(self, value):
+        sw = 1
+        i = 1
+        v = value
+        while True:
+            if self.contains(value) == 't':
+                False
+                return value
+            if sw == 1:
+                value = v - i
+                sw = 0
+            elif sw == 0:
+                value = v + i
+                i += 1
+
+
+    def minimum_value(self):
+        min = self.root
+
+        while True:
+            if min.left == None:
+                False
+                return min.value       
+            min = min.left
 
                 
             
@@ -74,9 +104,15 @@ my_tree.insert(29)
 my_tree.insert(14)
 my_tree.insert(11)
 
+print("\nSi se agrega de nuevo el 54: ")
+my_tree.insert(54)
 
 my_tree.contains(4)
 my_tree.contains(29)
+
+print(f"El valor mas cercano al 12 es: {my_tree.closest_value(12)}\n")
+
+print(f"El valor minimo dentro del arbol es: {my_tree.minimum_value()}\n")
 
 my_tree.print()
 
